435a17b17f7046f466b8569416b583a6610a59eb
nivyapeter <nivyakpeter@gmail.com>
2021-11-19 23:44:04 +0530

    banner section

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
 <body>
     <header id="news-head" class="header-site bg-cyan-300 ">
         <div class="news-main-head">
+            <div class="main-text">
             <p class="news-title">News@Media</p>
             <p>Mysoor road, Sulthan bathery</p>
+            </div>
             <nav class="nav-bar">
                 <ul class="main-nav">
                     <li class="nav-items">
@@ -30,10 +32,32 @@
     </header>
     <section id="banner">
         <div class="col-md-12 col-sm-12 col-xs-12 banner-section" id="banner-page">
+            <div class="container">
+                <div class="row text-center">
+                    <h1>Welcome to News@Media</h1>
+                    <br>
+                    <p>That every day is a new day, and it's never too late to fix something</p>
+                </div>
+            </div>
 
         </div>
 
     </section>
+    <!-- news grid -->
+    <section id="newsgrid">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
 
 
 
