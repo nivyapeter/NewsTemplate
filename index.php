644bfb66645928c2abb9e0358443f8dbfc0cbd40
nivyapeter <nivyakpeter@gmail.com>
2021-11-24 23:29:52 +0530

    style:styling issues in grid

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
     <header id="news-head" class="header-site bg-cyan-300 ">
         <div class="news-main-head">
             <div class="main-text">
-            <p class="news-title">News@Media</p>
-            <p>Mysoor road, Sulthan bathery</p>
+                <p class="news-title">News@Media</p>
+                <p>Mysoor road, Sulthan bathery</p>
             </div>
             <nav class="nav-bar">
                 <ul class="main-nav">
@@ -35,8 +35,8 @@
         <div class="col-md-12 col-sm-12 col-xs-12 banner-section" id="banner-page">
             <div class="title-container">
                 <!-- <div class="row center-text"> -->
-                    <h1>Welcome to News@Media</h1>
-                    <p>That every day is a new day, and it's never too late to fix something</p>
+                <h1>Welcome to News@Media</h1>
+                <p>That every day is a new day, and it's never too late to fix something</p>
                 <!-- </div> -->
             </div>
 
@@ -47,17 +47,17 @@
     <section id="newsgrid">
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="container">
-            <?php
-for ($x = 0; $x <= 10; $x++) {
-   echo "<div class='news-box'>
-   <div>
-   <img class='news-img' src='./img/download.jpeg'>
-    </div>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aliquid? Consequuntur molestiae necessitatibus, amet cumque autem architecto totam inventore aliquid.</P>
-   </div>";
-}
-?>
-                
+               <?php
+                    for ($x = 0; $x <= 5; $x++) {
+                    echo "<div class='news-box'>
+                    <div>
+                    <img class='news-img' src='./img/download.jpeg'>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aliquid? Consequuntur molestiae necessitatibus, amet cumque autem architecto totam inventore aliquid.</P>
+                    </div>";
+                    }
+               ?>
+
             </div>
         </div>
     </section>
