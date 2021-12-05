a3a617483205011f21d9f196e63f6f1ef934eae2
nivyapeter <nivyakpeter@gmail.com>
2021-12-05 23:55:31 +0530

    style:adding styles to form

--- home.php
+++ home.php
@@ -7,13 +7,40 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
+<body class="bg-gray-500">
     <!-- html form -->
-    <form action="#" method="post">
-          Title: <input type="text" name="name"><br>
-          Image: <input type="text" name="email"><br>
-          Description: <input type="text" name="email"><br>
-          <input type="submit">
+ <div class="wrapper border-black-200 background-gray-300 flex justify-center background-gray-500">
+    <form class="w-full max-w-lg" action="#" method="post">
+        <h1 class="text-blue-300 text-center font-20 mt-40">Data Submission Form</h1>
+        <div class="flex flex-wrap mb-6 justify-center">
+            <div class="w-full  px-3 mb-6 ">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-5" for="title">
+                   Title
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="title" type="text">
+                <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
+            </div>
+            <div class="w-full  px-3 mb-6 >
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-5" for="image">
+                   Image
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="image" type="text">
+               
+            </div>
+            <div class="w-full  px-3 mb-6 ">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-5" for="description">
+                   Description
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="description" type="text">
+               
+            </div>
+            
+            <button class="shadow bg-blue-500 mt-2 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                  Submit
+            </button>
+        </div>
+
     </form>
+</div>
 </body>
 </html>
