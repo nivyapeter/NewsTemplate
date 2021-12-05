873fcc7edddab335d5446cae4689b5287f164126
nivyapeter <nivyakpeter@gmail.com>
2021-12-05 23:14:11 +0530

    form for the submision of data

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <!-- html form -->
+    <form action="#" method="post">
+          Title: <input type="text" name="name"><br>
+          Image: <input type="text" name="email"><br>
+          Description: <input type="text" name="email"><br>
+          <input type="submit">
+    </form>
+</body>
+</html>
