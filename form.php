9e197f12d2531071b82edc38eda4d21a4f2a41b6
nivyapeter <nivyakpeter@gmail.com>
2021-12-07 22:55:58 +0530

    form submission

--- form.php
+++ form.php
@@ -1,19 +1,28 @@
 <?php
-
+ $servername = "localhost";
+ $db = "newstemplate";
+ $username = "root";
+ $password = "Vonnue@1234#";
+// connection
+ $conn = mysqli_connect($servername,$username,$password,$db);
+ if(!$conn) {
+     echo "not connected";
+ 
+ } 
+ 
+ 
 // submiting values 
-$ID =  $_REQUEST['id'];
-$IMAGE = $_REQUEST['image'];
-$DESCRIPTION =  $_REQUEST['description'];
-$TITLE = $_REQUEST['title'];
+$ID =  $_POST['id'];
+$IMAGE = $_POST['image'];
+$DESCRIPTION =  $_POST['description'];
+$TITLE = $_POST['title'];
 
-
-$sql = "INSERT INTO college  VALUES ('$ID', 
-            '$IMAGE','$DESCRIPTION','$TITLE')";
-          
-
-
-
-
-
-
+$sql = "INSERT INTO NEWS (ID, IMAGE, DESCRIPTION, TITLE)  VALUES ('1','2','3','4')";
+IF (mysqli_query($conn,$sql)) {
+    echo "Recordes added successfully";
+} else {
+    echo "Not inserted";
+}
+    $conn->close();      
 ?>
+ 
--- home.php
+++ home.php
@@ -42,10 +42,11 @@
                
             </div>
             
-            <button class="shadow bg-blue-500 mt-2 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+            <button class="shadow bg-blue-500 mt-2 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                   Submit
             </button>
         </div>
+        
 
     </form>
 </div>
